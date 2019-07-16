221815f83182d02f40b77709e7d7e4b2e214afed
Harum Helmy <harumendhah@gmail.com>
2019-07-16 12:56:22 -0400

    passing TWO tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,36 @@
-
+require 'pry'
 require_relative '../../config/environment'
 
 class ApplicationController < Sinatra::Base
 
+
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :show_exceptions => false
   end
 
   get '/' do
+
+  end
+
+  get '/articles' do
+    @articles = Article.all 
+    erb :index
   end
 
   get '/articles/new' do 
     erb :new
   end
-  post '/articles' do
 
-
-    redirect "/show" 
-
+  get '/articles/:id' do
+    id = params[:id]
+    @article = Article.find[id]
+    erb :show
   end
 
+
+
+  # post '/articles' do
+  # end
 end
