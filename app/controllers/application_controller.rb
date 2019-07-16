98c2b47d642534bce42db2d73fd6bfa90466073e
Trevahr Robertson <Trevahr@gmail.com>
2019-07-16 11:58:07 -0400

    stopping1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,30 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    erb :index
   end
+
+  get '/articles' do
+     @articles = Article.all
+     erb :index
+  end
+
+  post '/articles' do
+    @title = params[:title]
+    @content = params[:content]
+    @new_article = Article.create(title: @title, content: @content)
+    redirect "/articles/#{@new_article.id}"
+  end
+
+  get '/articles/new' do
+  erb :new
+  end
+
+  get '/articles/:id' do
+    id = params[:id]
+    @article = Article.find(id)
+    erb :show
+  end
+
+
 end
