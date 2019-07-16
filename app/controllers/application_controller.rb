c2b6416001f925a27c5a644f4cf876c71c6a0215
github username <github email address>
2019-07-16 12:14:40 -0400

    first commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,15 @@ class ApplicationController < Sinatra::Base
 
   get '/' do
   end
+
+  get '/articles/new' do 
+    erb :new
+  end
+  post '/articles' do
+
+
+    redirect "/show" 
+
+  end
+
 end
